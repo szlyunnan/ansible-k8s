bb7249650fa0b20b5706ac74af0af5a46bcc202f
shenzhonglin <2522845282@qq.com>
2019-03-18 14:23:00 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ansible-k8s
 > ansible 2.6.2 部署 kubernetes 版本
+
+```
+    功能实现:
+        证书
+        域名解析
+        kubernetes群集
+        etcd群集
+        添加kubernetes work 节点
+```
---
+++ install_ansible.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ansible_repo="https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.2-1.el7.ans.noarch.rpm"
+
+function _download_install_repo() {
+  yum -y install wget
+  wget /tmp/ansible-2.6.2.repo.rpm
+  yum -y localinstall /tmp/ansible-2.6.2.repo.rpm
+  yum makecache
+}
+
+function _install_ansible() {
+  yum -y install ansible
+}
+
+_download_install_repo
+_install_ansible
